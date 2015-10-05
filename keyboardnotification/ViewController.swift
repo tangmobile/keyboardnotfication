86e299ca41ed086815398522f8cbbba53deb1d48
L73 <tanglee81@gmail.com>
2015-10-04 22:21:38 -0500

    Initial Commit

---
+++ keyboardnotification/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  keyboardnotification
+//
+//  Created by Lee Tang on 10/4/15.
+//  Copyright © 2015 com.Tang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
